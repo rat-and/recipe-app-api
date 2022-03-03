b28a040ba71d47c865b23051897bacabe02e3d65
Andre DESKTOP-3HEJK6B <jatar00@onet.eu>
2022-03-03 20:25:10 +0100

    [UT] Added endpoint for creating and authenticating users

---
+++ Makefile
@@ -0,0 +1,55 @@
+CURR_PATH := $(shell pwd)
+APP_DIR = $(shell echo "app")
+APP_NAME = $(shell echo "recipe-app-api")
+
+.PHONY: prep-dev-env-win
+prep-dev-env-win:  ## Prepares dev environment for windows os
+	@powershell Set-ExecutionPolicy RemoteSigned && [Environment]::Is64BitProcess
+
+.PHONY: build-dev-env
+build-dev-env:  ## Build dev instance on your local machine
+	@docker-compose \
+ 		--file docker-compose.yml \
+ 		--project-name $(APP_NAME) \
+ 		build
+	@echo "[DEV-INFO] DEV instances were successfully built!"
+
+.PHONY: run-dev-env
+run-dev-env:  ## Run DEV instance
+	@docker-compose \
+		--file docker-compose.yml run \
+		--rm \
+		--detach \
+		app
+	@echo "[DEV-INFO] DEV instances were successfully started!"
+
+.PHONY: run-unit-tests
+run-unit-tests:  ## Run unit tests on DEV instance
+	@docker-compose \
+		--file docker-compose.yml run \
+		--rm \
+		app \
+		sh -c "python manage.py test && flake8"
+	@docker-compose \
+		--file docker-compose.yml \
+ 		down --remove-orphans
+	@echo "[DEV-INFO] Unit Tests on DEV instance(s) were successfully run!"
+
+.PHONY: run-ut-with-coverage
+run-ut-with-coverage:  ## Run unit tests with coverage on DEV instance
+	@docker-compose \
+		--file docker-compose.yml run \
+		--rm \
+		app \
+		sh -c "coverage run --source='.' manage.py test && coverage report > .coverage.report && coverage erase && cat .coverage.report"
+	@docker-compose \
+		--file docker-compose.yml \
+ 		down --remove-orphans
+	@echo "[DEV-INFO] Unit Tests on DEV instance(s) were successfully run! Coverage report in ./app/.coverage.report"
+
+.PHONY: down-dev-env
+down-dev-env:  ## Stop and clear DEV instance
+	@docker-compose \
+		--file docker-compose.yml \
+ 		down --remove-orphans
+	@echo "[DEV-INFO] DEV instances were successfully stopped."
--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -5,8 +5,8 @@ from django.urls import reverse
 from rest_framework.test import APIClient
 from rest_framework import status
 
-
 CREATE_USER_URL = reverse('user:create')
+TOKEN_URL = reverse('user:token')
 
 
 def create_user(**param):
@@ -24,7 +24,7 @@ class PublicUserApiTests(TestCase):
         payload = {
             'email': 'test@dummy.com',
             'password': 'testpass',
-            'name': 'Dummy Dumb',
+            'name': 'Valid User',
         }
         res = self.client.post(CREATE_USER_URL, payload)
 
@@ -38,18 +38,18 @@ class PublicUserApiTests(TestCase):
         payload = {
             'email': 'test@dummy.com',
             'password': 'testpass',
+            'name': 'Existing User',
         }
         create_user(**payload)
-
         res = self.client.post(CREATE_USER_URL, payload)
-
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_password_too_short(self):
         """TEST that the password must be more than 5 characters"""
         payload = {
             'email': 'test@dummy.com',
-            'password': 'pw',
+            'password': 'pwd',
+            'name': 'Short Password',
         }
 
         res = self.client.post(CREATE_USER_URL, payload)
@@ -59,3 +59,58 @@ class PublicUserApiTests(TestCase):
             email=payload['email']
         ).exists()
         self.assertFalse(user_exists)
+
+    def test_create_token_for_user(self):
+        """Test that a token is created for the user"""
+        payload = {
+            'email': 'test@dummy.com',
+            'password': 'testpass',
+            'name': 'Create Token',
+        }
+        create_user(**payload)
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_create_token_invalid_credentials(self):
+        """Test that token is not created if invalid credentials are given"""
+        payload_ok = {
+            'email': 'test@dummy.com',
+            'password': 'testpass',
+            'name': 'Invalid Creds',
+        }
+        payload_nok = {
+            'email': 'test@dummy.com',
+            'password': 'wrongpass',
+            'name': 'Invalid Creds',
+        }
+        create_user(**payload_ok)
+        res = self.client.post(TOKEN_URL, payload_nok)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_no_user(self):
+        """Test that token is not created if user doesn't exist"""
+        payload = {
+            'email': 'test@dummy.com',
+            'password': 'testpass',
+            'name': 'No User',
+        }
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_missing_field(self):
+        """Test that email and password are required"""
+        payload = {
+            'email': 'dummy',
+            'password': '',
+            'name': 'Invalid Creds',
+        }
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
