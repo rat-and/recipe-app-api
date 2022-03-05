57136adb923b9bbaa765750d2e54bc534c5161da
Andre DESKTOP-3HEJK6B <jatar00@onet.eu>
2022-03-05 16:44:27 +0100

    Added working list tags endpoint

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,12 +1,18 @@
 from django.test import TestCase
-
 from django.contrib.auth import get_user_model
+
+from core import models
+
+
+def sample_user(email='test@dummy.com', password='testpass'):
+    """Create a sample user"""
+    return get_user_model().objects.create_user(email, password)
 
 
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
-        """ ***TEST creating a new user with an email is successful """
+        """Test creating a new user with an email is successful """
         email = "dummy@dummy.com"
         password = "dummyPASSWORD"
         user = get_user_model().objects.create_user(
@@ -18,19 +24,19 @@ class ModelTests(TestCase):
         self.assertTrue(user.check_password(password))
 
     def test_new_user_email_normalized(self):
-        """ ***TEST if the email for a new user is normalized """
+        """Test if the email for a new user is normalized"""
         email = "test@DUMMY.COM"
         user = get_user_model().objects.create_user(email, "test123")
 
         self.assertEqual(user.email, email.lower())
 
     def test_new_user_invalid_email(self):
-        """ ***TEST if user with no email raises error"""
+        """Test if user with no email raises error"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, "test123")
 
     def test_create_new_superuser(self):
-        """ ***TEST creating a new superuser"""
+        """Test creating a new superuser"""
         user = get_user_model().objects.create_superuser(
             "dummy@dummy.com",
             "dummyPASS123"
@@ -38,3 +44,12 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """Test the tag string representation"""
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name="TestTag",
+        )
+
+        self.assertEqual(str(tag), tag.name)
